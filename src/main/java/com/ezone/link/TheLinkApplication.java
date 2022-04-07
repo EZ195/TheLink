85022f5f9794b59d4b0b95f6ae6563f7fa7d667b
LAPTOP-JPK2Q002\EZi09 <95jiwon91@gmail.com>
2022-04-07 17:46:59 +0900

    최초 업로드

---
+++ src/main/java/com/ezone/link/TheLinkApplication.java
@@ -0,0 +1,13 @@
+package com.ezone.link;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheLinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheLinkApplication.class, args);
+	}
+
+}
