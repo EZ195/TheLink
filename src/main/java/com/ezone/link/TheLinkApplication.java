b141079b275fd5518d9e19742151bad618ea69dd
LAPTOP-JPK2Q002\EZi09 <95jiwon91@gmail.com>
2022-05-10 15:54:14 +0900

    [이지원]
    - 배포 위한 코드 추가

--- src/main/java/com/ezone/link/TheLinkApplication.java
+++ src/main/java/com/ezone/link/TheLinkApplication.java
@@ -2,9 +2,10 @@ package com.ezone.link;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class TheLinkApplication {
+public class TheLinkApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TheLinkApplication.class, args);
--- src/main/java/com/ezone/link/common/FileManagerService.java
+++ src/main/java/com/ezone/link/common/FileManagerService.java
@@ -12,7 +12,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 public class FileManagerService {
 	
-	public final static String FILE_UPLOAD_PATH = "C:\\Users\\EZi09\\Desktop\\ezone\\theLink\\upload\\profileImage/";
+	public final static String FILE_UPLOAD_PATH = "/home/ec2-user/thelink/upload/images";
 	
 	private static Logger logger = LoggerFactory.getLogger(FileManagerService.class);
 	
