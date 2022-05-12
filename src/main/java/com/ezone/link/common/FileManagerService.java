e652395aab9f927592c241464164a578aa2d29e0
LAPTOP-JPK2Q002\EZi09 <95jiwon91@gmail.com>
2022-05-12 16:16:34 +0900

    파일 경로 재수정

--- src/main/java/com/ezone/link/common/FileManagerService.java
+++ src/main/java/com/ezone/link/common/FileManagerService.java
@@ -12,7 +12,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 public class FileManagerService {
 	
-	public final static String FILE_UPLOAD_PATH = "/home/ec2-user/thelink/upload/profileImage";
+	public final static String FILE_UPLOAD_PATH = "/home/ec2-user/thelink/upload/profileImage/";
 
 	private static Logger logger = LoggerFactory.getLogger(FileManagerService.class);
 	
